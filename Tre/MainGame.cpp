e36aeb9c8453e7b645043b7fde3ec30826cf286c
YAP! <GMADDA200@caledonian.ac.uk>
2017-03-20 21:57:23 +0000

    initial commit

---
+++ Tre/MainGame.cpp
@@ -0,0 +1,92 @@
+#include "MainGame.h"
+#include "Camera.h"
+#include <iostream>
+#include <string>
+
+
+
+Vertex vertices[] = { Vertex(glm::vec3(-0.5, -0.5, 0), glm::vec2(0.0, 0.0)),
+					Vertex(glm::vec3(0, 0.5, 0), glm::vec2(0.5, 1.0)),
+					Vertex(glm::vec3(0.5, -0.5, 0), glm::vec2(1.0, 0.0)) };
+
+unsigned int indices[] = { 0, 1, 2 };
+
+Transform transform;
+
+MainGame::MainGame()
+{
+	_gameState = GameState::PLAY;
+	Display* _gameDisplay = new Display(); //new display
+    Mesh* mesh1();
+	Mesh* mesh2();
+}
+
+MainGame::~MainGame()
+{
+}
+
+void MainGame::run()
+{
+	initSystems(); 
+	gameLoop();
+}
+
+void MainGame::initSystems()
+{
+	_gameDisplay.initDisplay(); 
+	//mesh1.init(vertices, sizeof(vertices) / sizeof(vertices[0]), indices, sizeof(indices) / sizeof(indices[0])); //size calcuated by number of bytes of an array / no bytes of one element
+	mesh2.loadModel("../res/monkey3.obj");
+	myCamera.initCamera(glm::vec3(0, 0, -5), 70.0f, (float)_gameDisplay.getWidth()/_gameDisplay.getHeight(), 0.01f, 1000.0f);
+	counter = 0.0f;
+}
+
+void MainGame::gameLoop()
+{
+	while (_gameState != GameState::EXIT)
+	{
+		processInput();
+		drawGame();
+	}
+}
+
+void MainGame::processInput()
+{
+	SDL_Event evnt;
+
+	while(SDL_PollEvent(&evnt)) //get and process events
+	{
+		switch (evnt.type)
+		{
+			case SDL_QUIT:
+				_gameState = GameState::EXIT;
+				break;
+		}
+	}
+	
+}
+
+
+void MainGame::drawGame()
+{
+	_gameDisplay.clearDisplay(0.0f, 0.0f, 0.0f, 1.0f);
+	
+	Shader shader("../res/shader"); //new shader
+	Texture texture("../res/bricks.jpg"); //load texture
+	Texture texture1("../res/water.jpg"); //load texture
+	
+	transform.SetPos(glm::vec3(sinf(counter), 0.0, 0.0));
+	transform.SetRot(glm::vec3(0.0, 0.0, counter * 5));
+	transform.SetScale(glm::vec3(sinf(counter), sinf(counter), sinf(counter)));
+
+	shader.Bind();
+	shader.Update(transform, myCamera);
+	texture.Bind(0);
+	//mesh1.draw();
+	mesh2.draw();
+	counter = counter + 0.01f;
+				
+	glEnableClientState(GL_COLOR_ARRAY); 
+	glEnd();
+
+	_gameDisplay.swapBuffer();
+} 
